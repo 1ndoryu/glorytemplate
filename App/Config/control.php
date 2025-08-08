8645826e0a87faa96eb8570c0708b009efe9c4e7
1ndoryu <andoryyu@gmail.com>
2025-08-08 16:48:28 -0400

    Elimina un espacio en blanco innecesario en el archivo de configuración control.php, mejorando la limpieza del código.

--- App/Config/control.php
+++ App/Config/control.php
@@ -2,29 +2,49 @@
 
 use Glory\Core\GloryFeatures;
 
-
 //Managers
 GloryFeatures::enable('menu');
+GloryFeatures::enable('assetManager');
+GloryFeatures::enable('opcionManagerSync');
+GloryFeatures::enable('syncManager');
+GloryFeatures::enable('gloryLogger');
+GloryFeatures::enable('defaultContentManager');
+GloryFeatures::enable('pageManager');
+GloryFeatures::enable('postTypeManager');
+GloryFeatures::enable('taxonomyMetaManager');
+GloryFeatures::enable('scheduleManager');
 
 //UI Components
-GloryFeatures::disable('modales');
-GloryFeatures::disable('submenus');
-GloryFeatures::disable('pestanas');
-GloryFeatures::disable('scheduler');
-GloryFeatures::disable('headerAdaptativo');
-GloryFeatures::disable('alertas');
-GloryFeatures::disable('gestionarPreviews');
-GloryFeatures::disable('paginacion');
+GloryFeatures::enable('modales');
+GloryFeatures::enable('submenus');
+GloryFeatures::enable('pestanas');
+GloryFeatures::enable('scheduler');
+GloryFeatures::enable('headerAdaptativo');
+GloryFeatures::enable('alertas');
+GloryFeatures::enable('gestionarPreviews');
+GloryFeatures::enable('paginacion');
 
 //Services
 GloryFeatures::disable('cssCritico');
-GloryFeatures::disable('navegacionAjax');
-GloryFeatures::disable('gloryAjax');
-GloryFeatures::disable('gloryForm');
+GloryFeatures::enable('navegacionAjax');
+GloryFeatures::enable('gloryAjax');
+GloryFeatures::enable('gloryForm');
 GloryFeatures::disable('gloryBusqueda');
 
+// Renderers
+GloryFeatures::disable('logoRenderer');
+GloryFeatures::disable('contentRender');
+GloryFeatures::disable('termRender');
 
 
 
 
+
+
+
+
+
+
+GloryFeatures::disable('titleTag');
+GloryFeatures::disable('postThumbnails');
 
