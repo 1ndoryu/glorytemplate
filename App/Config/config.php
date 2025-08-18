f47287ae997bdd4505bee03506cc1c6cad8fe3ed
1ndoryu <andoryyu@gmail.com>
2025-08-17 20:49:13 -0400

    Actualiza .gitignore para incluir glory_docs y dev-tools. Modifica config.php para utilizar AjaxNav, simplificando la gestión de selectores de contenido y mejorando la legibilidad del código.

--- App/Config/config.php
+++ App/Config/config.php
@@ -3,6 +3,7 @@
 use Glory\Manager\AssetManager;
 use Glory\Manager\PageManager;
 use Glory\Admin\SyncManager;
+use Glory\Helpers\AjaxNav;
 
 AssetManager::setThemeVersion('0.1.1');
 SyncManager::setAdminBarVisible(true);
@@ -10,8 +11,6 @@ SyncManager::setResetButtonVisible(true);
 
 PageManager::define('home', 'home');
 
-add_filter('glory/nav_config', function(array $config): array {
-    $config['contentSelector'] = '#main';
-    $config['mainScrollSelector'] = '#main';
-    return $config;
-});
+AjaxNav::contentSelector('#main');
+AjaxNav::mainScrollSelector('#main');
+AjaxNav::registerFilter();
---
+++ App/Config/control.php
@@ -0,0 +1,45 @@
+<?php
+
+use Glory\Core\GloryFeatures;
+
+//Managers
+GloryFeatures::disable('menu');
+GloryFeatures::enable('assetManager');
+GloryFeatures::enable('opcionManagerSync');
+GloryFeatures::enable('syncManager');
+GloryFeatures::enable('gloryLogger');
+GloryFeatures::enable('pageManager');
+GloryFeatures::enable('postTypeManager');
+GloryFeatures::enable('taxonomyMetaManager');
+GloryFeatures::enable('scheduleManager');
+GloryFeatures::enable('defaultContentManager');
+GloryFeatures::disable('licenseManager');
+GloryFeatures::disable('creditosManager');
+
+//UI Components
+GloryFeatures::enable('modales');
+GloryFeatures::enable('submenus');
+GloryFeatures::enable('pestanas');
+GloryFeatures::enable('scheduler');
+GloryFeatures::disable('headerAdaptativo');
+GloryFeatures::enable('alertas');
+GloryFeatures::enable('gestionarPreviews');
+GloryFeatures::enable('paginacion');
+GloryFeatures::enable('gloryFilters');
+
+//Services
+GloryFeatures::disable('cssCritico');
+GloryFeatures::enable('navegacionAjax');
+GloryFeatures::enable('gloryAjax');
+GloryFeatures::enable('gloryForm');
+GloryFeatures::disable('gloryBusqueda');
+GloryFeatures::disable('gloryRealtime');
+
+// Renderers
+GloryFeatures::disable('logoRenderer');
+GloryFeatures::disable('contentRender');
+GloryFeatures::disable('termRender');
+
+//Theme options
+GloryFeatures::enable('titleTag');
+GloryFeatures::enable('postThumbnails');
