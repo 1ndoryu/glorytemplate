b37e7266facf6fdf62238c7edd52f3f1b4e1b2fb
1ndoryu <andoryyu@gmail.com>
2025-10-24 16:03:59 -0400

    Agregar estructura y estilos del footer con 4 secciones flex row

--- footer.php
+++ footer.php
@@ -4,6 +4,15 @@
 
 </main>
 
+<footer class="footer">
+    <div class="footerContenedor">
+        <div class="footerSeccion"></div>
+        <div class="footerSeccion"></div>
+        <div class="footerSeccion"></div>
+        <div class="footerSeccion"></div>
+    </div>
+</footer>
+
 <?php wp_footer(); ?>
 </body>
 
