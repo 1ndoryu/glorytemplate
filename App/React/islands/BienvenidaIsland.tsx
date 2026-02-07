d352c85bffc2d87ad7059204bc050c66f0db15ca
1ndoryu <andoryyu@gmail.com>
2026-02-07 13:50:54 -0400

    *

---
+++ App/React/islands/BienvenidaIsland.tsx
@@ -0,0 +1,23 @@
+/**
+ * Componente: Pagina de Bienvenida
+ * Muestra un mensaje simple centrado en pantalla.
+ * Caso de uso: Primera pagina de prueba.
+ */
+
+import React from 'react';
+import '../styles/variables.css';
+import '../styles/bienvenida.css';
+
+interface BienvenidaIslandProps {
+    titulo?: string;
+}
+
+export const BienvenidaIsland = ({titulo = 'Bienvenido a Glory React'}: BienvenidaIslandProps): JSX.Element => {
+    return (
+        <div className="contenedorBienvenida">
+            <h1 className="tituloBienvenida">{titulo}</h1>
+        </div>
+    );
+};
+
+export default BienvenidaIsland;
