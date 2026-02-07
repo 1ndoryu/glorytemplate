02e154f3a043d6a97f5876dcd095f3595032f9bf
1ndoryu <andoryyu@gmail.com>
2026-02-07 16:54:02 -0400

    style: unificar logos color negro y misma altura, mantener 6 columnas

--- App/React/components/home/SeccionClientes.tsx
+++ App/React/components/home/SeccionClientes.tsx
@@ -25,20 +25,6 @@ export const SeccionClientes: React.FC = () => {
                 {/* Header: Titulo y Boton de "Our Work" */}
                 <header className="clientesHeader">
                     <span className="clientesTitulo">Clients this year</span>
-
-                    <a href="/work" className="clientesBotonBox" aria-label="Ver nuestro trabajo">
-                        <div className="clientesBotonIcono">
-                            {/* Icono simple de ojo o similar */}
-                            <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12C23 12 19 20 12 20C5 20 1 12 1 12Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                <circle cx="12" cy="12" r="3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </div>
-                        <div className="clientesBotonTexto">
-                            <span className="clientesBotonTitulo">Our work</span>
-                            <span className="clientesBotonSubtitulo">See all of our work</span>
-                        </div>
-                    </a>
                 </header>
 
                 {/* Grid de Logos */}
